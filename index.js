db6821fd20c36d19a46013437827ef5602be9468
Ignacio Orlandini <ignacioorlandini@gmail.com>
2016-11-14 15:32:32 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,60 @@
+var _ = require('lodash');
+var moment = require('moment');
+
+module.exports = {
+  toJson: toJson
+};
+
+function toJson(fields, line) {
+  var json = {};
+
+  _.forEach(fields, function(field) {
+    var value = line.substr(0, field.width);
+
+    if (!field.formatFn) {
+      switch (field.type) {
+        case 'date':
+          value = new Date(moment(value, field.format).format());
+          if (isNaN(value.getTime()) && !_.isUndefined(field.default)) {
+            value = field.default;
+          }
+          break;
+
+        case 'float':
+          value = parseFloat(value);
+          if (isNaN(value) && !_.isUndefined(field.default)) {
+            value = field.default;
+          }
+          break;
+
+        case 'int':
+          value = parseInt(value);
+          if (isNaN(value) && !_.isUndefined(field.default)) {
+            value = field.default;
+          }
+          break;
+
+        default:
+          value = String(value);
+          if (field.trim !== false) {
+            value = value.trim();
+          }
+          if (!value && !_.isUndefined(field.default)) {
+            value = field.default;
+          }
+      }
+    } else if (typeof field.formatFn === 'function') {
+      value = field.formatFn(value, field);
+    } else {
+      var fn = global[field.formatFn];
+      if (typeof fn === 'function') {
+        value = fn(value, field);
+      }
+    }
+
+    _.set(json, field.level ? field.level + '.' + field.name : field.name, value);
+    line = line.substr(field.width);
+  });
+
+  return json;
+}
